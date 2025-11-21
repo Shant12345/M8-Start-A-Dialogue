fc88d4dc730607778e48f63f2e2ac09674bf4991
Shant <shdorsey@s.sfusd.edu>
2025-11-21 12:01:41 -0800

    M8L6

--- lessons/dialogue.gd
+++ lessons/dialogue.gd
@@ -1,51 +1,6 @@
 extends Control
 
-var expressions := {
-	"happy": preload ("res://assets/emotion_happy.png"),
-	"regular": preload ("res://assets/emotion_regular.png"),
-	"sad": preload ("res://assets/emotion_sad.png"),
-}
-
-var bodies := {
-	"sophia": preload ("res://assets/sophia.png"),
-	"pink": preload ("res://assets/pink.png")
-}
-
-var dialogue_items: Array[Dictionary] = [
-	{
-		"expression": expressions["regular"],
-		"text": "[wave]Hey, wake up![/wave]\nIt's time to play basketball.",
-		"character": bodies["sophia"],
-		"choices": {
-			"Let me sleep a little longer": 2,
-			"Let's do it!": 1,
-		},
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "Great! Your first task will be to make a three pointer.",
-		"character": bodies["sophia"],
-		"choices": {
-			"I will do my best": 3,
-			"No, let me go back to sleep": 2,
-		},
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "Oh, come on! It'll be fun.",
-		"character": bodies["pink"],
-		"choices": {
-			"No, really, let me go back to sleep": 0,
-			"Alright, I'll try": 1,
-		},
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "That's the spirit! [wave]You can do it![/wave]",
-		"character": bodies["pink"],
-		"choices": {"Okay! (Quit)": - 1},
-	},
-]
+@export var dialogue_items: Array[DialogueItem] = []
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
@@ -57,41 +12,42 @@ var dialogue_items: Array[Dictionary] = [
 func _ready() -> void:
 	show_text(0)
 
+
 func show_text(current_item_index: int) -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item["text"]
-	expression.texture = current_item["expression"]
-	body.texture = current_item["character"]
-	create_buttons(current_item["choices"])
 	rich_text_label.visible_ratio = 0.0
+	rich_text_label.text = current_item.text
+	expression.texture = current_item.expression
+	body.texture = current_item.character
+	create_buttons(current_item.choices)
 	var tween := create_tween()
-	var text_appearing_duration: float = current_item["text"].length() / 30.0
+	var text_appearing_duration := (current_item["text"] as String).length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
-
+	
 	slide_in()
-
 	for button: Button in action_buttons_v_box_container.get_children():
 		button.disabled = true
-	tween.finished.connect(func() -> void:
+	tween.finished.connect( func() -> void:
 		for button: Button in action_buttons_v_box_container.get_children():
 			button.disabled = false
 	)
 
-func create_buttons(choices_data: Dictionary) -> void:
+
+func create_buttons(choices_data: Array[DialogueChoice]) -> void:
 	for button in action_buttons_v_box_container.get_children():
 		button.queue_free()
-	for choice_text in choices_data:
+	for choice in choices_data:
 		var button := Button.new()
 		action_buttons_v_box_container.add_child(button)
-		button.text = choice_text
-		var target_line_idx: int = choices_data[choice_text]
-		if target_line_idx == - 1:
+		button.text = choice.text
+		if choice.is_quit == true:
 			button.pressed.connect(get_tree().quit)
 		else:
+			var target_line_idx := choice.target_line_idx
 			button.pressed.connect(show_text.bind(target_line_idx))
 
 func slide_in() -> void:
